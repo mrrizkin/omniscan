40632eb98ea7d615e8c0681010b2e4eda802a6fe
nugrhrizki <nugraha07rizki@gmail.com>
2024-12-10 11:28:32 +0700

    update: remove the expired eStatement

--- app/console/scheduler.go
+++ app/console/scheduler.go
@@ -2,9 +2,13 @@ package console
 
 import (
 	"github.com/mrrizkin/omniscan/app/providers/scheduler"
+	"github.com/mrrizkin/omniscan/app/repositories"
 )
 
-func Schedule(schedule *scheduler.Scheduler) {
+func Schedule(
+	schedule *scheduler.Scheduler,
+	eStatementRepository *repositories.EStatementRepository,
+) {
 	// Example usage:
 	// schedule.Add("@every 1m", func() {
 	// 	log.Info("Scheduled task ran", "at", time.Now().Format(time.RFC3339))
@@ -12,4 +16,9 @@ func Schedule(schedule *scheduler.Scheduler) {
 	//
 	// Add your scheduled tasks here. Use cron syntax for defining intervals.
 	// Refer to the scheduler documentation for more advanced usage.
+
+	// deleting expired e-statements
+	schedule.Add("@every 1m", func() {
+		eStatementRepository.Bomb()
+	})
 }
