8389dad635be5fbf75366571eb3f968893e50972
nugrhrizki <nugraha07rizki@gmail.com>
2024-12-11 14:39:38 +0700

    update: improve decoding font

--- pkg/pdf/provider/pdfcpu/parser.go
+++ pkg/pdf/provider/pdfcpu/parser.go
@@ -74,7 +74,7 @@ func (p *PDFCPU) parse(r io.Reader) ([]TextObject, error) {
 					state.CurrentTextObject.Text += encoder.Utf16Decode(textMatch[1][2:])
 				} else {
 					if state.CurrentFont != nil {
-						state.CurrentTextObject.Text += state.CurrentFont.Decode(textMatch[1])
+						state.CurrentTextObject.Text += state.CurrentFont.Decode(sanitizeIdentityH(line))
 					} else {
 						state.CurrentTextObject.Text += textMatch[1]
 					}
@@ -118,4 +118,95 @@ func (p *PDFCPU) parse(r io.Reader) ([]TextObject, error) {
 	}
 
 	return result, nil
+}
+
+func sanitizeIdentityH(text string) string {
+	text = strings.TrimSpace(text)
+	if strings.HasPrefix(text, "(") {
+		text = text[1:]
+	}
+	if strings.HasSuffix(text, "Tj") {
+		text = text[:len(text)-2]
+	}
+	text = strings.TrimSpace(text)
+	if strings.HasSuffix(text, ")") {
+		text = text[:len(text)-1]
+	}
+
+	tmp := []byte("")
+	reader := strings.NewReader(text)
+	depth := 1
+Loop:
+	for {
+		if reader.Len() == 0 {
+			break
+		}
+
+		c, err := reader.ReadByte()
+		if err != nil {
+			break
+		}
+
+		switch c {
+		default:
+			tmp = append(tmp, c)
+		case '(':
+			depth++
+			tmp = append(tmp, c)
+		case ')':
+			if depth--; depth == 0 {
+				break Loop
+			}
+			tmp = append(tmp, c)
+		case '\\':
+			c, err := reader.ReadByte()
+			if err != nil {
+				break
+			}
+			switch c {
+			default:
+				reader.UnreadByte()
+				tmp = append(tmp, '\\', c)
+			case 'n':
+				tmp = append(tmp, '\n')
+			case 'r':
+				tmp = append(tmp, '\r')
+			case 'b':
+				tmp = append(tmp, '\b')
+			case 't':
+				tmp = append(tmp, '\t')
+			case 'f':
+				tmp = append(tmp, '\f')
+			case '(', ')', '\\':
+				tmp = append(tmp, c)
+			case '\r':
+				if innerC, err := reader.ReadByte(); err == nil {
+					if innerC != '\n' {
+						reader.UnreadByte()
+					}
+				}
+				fallthrough
+			case '\n':
+				// no append
+			case '0', '1', '2', '3', '4', '5', '6', '7':
+				x := int(c - '0')
+				for i := 0; i < 2; i++ {
+					if c, err := reader.ReadByte(); err == nil {
+						if c < '0' || c > '7' {
+							reader.UnreadByte()
+							break
+						}
+						x = x*8 + int(c-'0')
+					}
+				}
+				if x > 255 {
+					reader.UnreadByte()
+					break
+				}
+				tmp = append(tmp, byte(x))
+			}
+		}
+	}
+
+	return string(tmp)
 }
